d9cd000ea6c310dbbb520c4c18dc8d6fbf7ad8a6
Felix Barros <felix.ricardo.barros@gmail.com>
2014-07-07 11:14:44 -0300

    Enlazamos el framework jQuery

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
         <p>Desarrollado con <span title="Amor">&#10084;</span> por <a href="http://adaptarme.com.ar">Adapar.ME</a></p>
     </footer>
 </div><!-- .container -->
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.flexslider.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.pagescroll2id.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.validate.js"></script>
